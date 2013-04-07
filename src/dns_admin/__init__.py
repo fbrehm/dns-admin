9091a0df6687bf6f9b648c0839064d52a646b471
Frank Brehm <frank.brehm@profitbricks.com>
2013-04-07 11:36:18 +0200

    First Python stuff

---
+++ src/dns_admin/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: © 2010 - 2013 by Frank Brehm, Berlin
+@summary: All modules for the DNS administration tool
+"""
+
+__author__ = 'Frank Brehm <frank.brehm@profitbricks.com>'
+__copyright__ = '(C) 2010-2013 by profitbricks.com'
+__contact__ = 'frank.brehm@profitbricks.com'
+__version__ = '0.0.1'
+__license__ = 'LGPLv3+'
+
+
+#==============================================================================
+
+if __name__ == "__main__":
+    pass
+
+#==============================================================================
+# vim: fileencoding=utf-8 filetype=python ts=4
