4b84477c1e0d43bf61e7835a8d68248f885e6085
Frank Brehm <frank.brehm@profitbricks.com>
2013-04-19 22:46:15 +0200

    Added src/dns_admin/db/__init__.py

---
+++ src/dns_admin/db/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank.brehm@profitbricks.com
+@copyright: © 2010 - 2013 by Frank Brehm, Berlin
+@summary: All encapsulation modules of database objects
+"""
+
+
+#==============================================================================
+
+if __name__ == "__main__":
+    pass
+
+#==============================================================================
+# vim: fileencoding=utf-8 filetype=python ts=4
